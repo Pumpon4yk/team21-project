a35fcb11f3279c3928963fd45cf03fad7567ee64
Pumpon4yk <tt10392@gmail.com>
2022-10-14 01:10:16 +0200

    general settings

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/*
+|=========================>
+|   Приклад на  CodePen "https://codepen.io/dan_postnov/pen/gjBqPb" як добавляти модалки 
+|   + в вебінарі є остатньому 
+|   + інструкція "https://medium.com/@dan.postnov/%D0%BC%D0%BE%D0%B4%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5-%D0%BE%D0%BA%D0%BD%D0%B0-%D0%BD%D0%B0-javascript-30-%D1%81%D1%82%D1%80%D0%BE%D0%BA-%D0%BA%D0%BE%D0%B4%D0%B0-dbbb599649f3"
+|=========================>
+*/
+import "./js/modal30"
