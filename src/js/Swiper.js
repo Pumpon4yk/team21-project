26c32521f030cc577d46434435bbfabfe019ed66
altabib <andrushchenko.ivan85@gmail.com>
2022-10-18 00:13:19 +0300

    gallery

---
+++ src/js/Swiper.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
