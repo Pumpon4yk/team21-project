3c885a46dd21f15f422fd2fd46555a19d0cd3902
Pumpon4yk <tt10392@gmail.com>
2022-10-19 23:59:44 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import "./js/mobile-menu.js";
-import "./js/header-scroll.js";
+import "./js/mobile-menu";
+import "./js/header-scroll";
+import "./js/scroll-sl";
 import "./js/Swiper";
-import "./js/reviews-slide"
+import "./js/reviews-slide";
 
 
 
@@ -12,4 +13,5 @@ import "./js/reviews-slide"
 |   + інструкція "https://medium.com/@dan.postnov/%D0%BC%D0%BE%D0%B4%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5-%D0%BE%D0%BA%D0%BD%D0%B0-%D0%BD%D0%B0-javascript-30-%D1%81%D1%82%D1%80%D0%BE%D0%BA-%D0%BA%D0%BE%D0%B4%D0%B0-dbbb599649f3"
 |=========================>
 */
-import "./js/modal30"
+import "./js/modal30";
+import "./js/modal";
